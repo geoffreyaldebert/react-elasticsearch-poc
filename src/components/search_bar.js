e9da96c75d3d6b8b3fcfa08f157cea444a85f266
blacktop <blacktop@users.noreply.github.com>
2017-11-24 14:48:58 -0700

    clean it up

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class SearchResults extends Component {
   render() {
     const results = this.props.results;
 
-    return (
-      <div className="search_results">
-        <hr />
-        <ul>
-          {results.map(result => {
-            return (
-              <li key={result._id}>
-                {result._source.plugins.av.clamav.result}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+    return (<div className="search_results">
+      <hr/>
+      <ul>
+        {
+          results.map(result => {
+            return (<li key={result._id}>
+              {result._source.plugins.av.clamav.result}
+            </li>);
+          })
+        }
+      </ul>
+    </div>);
   }
 }
 
