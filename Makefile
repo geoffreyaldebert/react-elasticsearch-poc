84bc46c32710dde2b45f3d6aead1ae12e1a36bb8
blacktop <blacktop@users.noreply.github.com>
2017-11-24 14:45:31 -0700

    add some data

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
+ELASTIC?=5.6
+
 .PHONY: start
 start: stop
-	@docker run -d --name elasticsearch -p 9200:9200 -e http.cors.enabled=true -e http.cors.allow-origin="/https?:\/\/localhost(:[0-9]+)?/" blacktop/elasticsearch:5.6
+	@echo "===> Starting elasticsearch..."
+	@docker run -d --name elasticsearch -p 9200:9200 -e http.cors.enabled=true -e http.cors.allow-origin="/https?:\/\/localhost(:[0-9]+)?/" blacktop/elasticsearch:$(ELASTIC);sleep 15
+	@echo "===> Adding data to elasticsearch..."
+	@cd scripts; ./add_data.sh
 	@open -a Brave http://localhost:8080/webpack-dev-server/
 	@npm run start
 
 .PHONY: stop
 stop:
+	@echo "===> Stopping elasticsearch..."
 	@docker rm -f elasticsearch || true
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import elasticsearch from "elasticsearch";
+import SearchResults from './search_bar';
+let client = new elasticsearch.Client({host: "localhost:9200", log: "trace"});
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: []
+    };
+  }
+
+  handleChange(event) {
+    const search_query = event.target.value;
+
+    client.search({q: search_query}).then(function(body) {
+      this.setState({results: body.hits.hits});
+    }.bind(this), function(error) {
+      console.trace(error.message);
+    });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+    <div className="container">
+        <input type="text" onChange={this.handleChange} />
+        <SearchResults results={this.state.results} />
+      </div>
     );
   }
 }
